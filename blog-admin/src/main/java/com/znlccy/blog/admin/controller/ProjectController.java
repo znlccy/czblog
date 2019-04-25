3662bd83df7a20709436332c645d37b63e32dd0b
chen.congye@trs.com.cn <chen.congye@trs.com.cn>
2019-04-25 15:39:02 +0800

    创建基本的Controller

---
+++ blog-admin/src/main/java/com/znlccy/blog/admin/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.znlccy.blog.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * ClassName: ProjectController
+ * FileName：ProjectController.java
+ * Description：项目控制器
+ * History：
+ * 版本号 			作者 			日期       				简介
+ * 	1.0				ccy		        2019/4/25			    create
+ */
+@Controller
+public class ProjectController {
+
+}
