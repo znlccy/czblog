926c1ed86644357abd4b038cb82deff0632cccbc
chen.congye@trs.com.cn <chen.congye@trs.com.cn>
2019-05-10 10:06:34 +0800

    添加各种工具类 v2

--- blog-core/src/main/java/com/znlccy/blog/core/util/MapCache.java
+++ blog-core/src/main/java/com/znlccy/blog/core/util/MapCache.java
@@ -1,13 +1,12 @@
 package com.znlccy.blog.core.util;
 
 /**
- * ClassName: Tools
- * FileName：Tools.java
- * Description：工具类
+ * ClassName: MapCache
+ * FileName：MapCache.java
+ * Description：缓存工具
  * History：
  * 版本号 			作者 			日期       				简介
  * 	1.0				ccy		        2019/5/10			    create
  */
-public class Tools {
-
+public class MapCache {
 }
--- blog-core/src/main/java/com/znlccy/blog/core/util/TaleUtils.java
+++ blog-core/src/main/java/com/znlccy/blog/core/util/TaleUtils.java
@@ -1,13 +1,13 @@
 package com.znlccy.blog.core.util;
 
 /**
- * ClassName: Tools
- * FileName：Tools.java
- * Description：工具类
+ * ClassName: TaleUtils
+ * FileName：TaleUtils.java
+ * Description：电话邮箱工具
  * History：
  * 版本号 			作者 			日期       				简介
  * 	1.0				ccy		        2019/5/10			    create
  */
-public class Tools {
+public class TaleUtils {
 
 }
